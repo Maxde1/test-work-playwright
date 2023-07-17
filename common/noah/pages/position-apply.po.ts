3c709764ef22a45ee9c4c54000ad59624a894fa3
Maksym Denysenko <maxde@gmail.com>
2023-07-17 17:31:47 +0300

    add position-apply page and locators

--- common/noah/pages/position-apply.po.ts
+++ common/noah/pages/position-apply.po.ts
@@ -1,9 +1,23 @@
 import { PositionApplyLocators } from '../locators/position-apply.locators'
-import { Page } from '@playwright/test'
+import { Page, PlaywrightTestArgs } from '@playwright/test'
+
+export const positionApplyPage = {
+    positionApplyPage : async (
+        {page}: PlaywrightTestArgs,
+        use: (r: PositionApplyPage ) => void
+    ) => {
+        const positionApplyPage  = new PositionApplyPage(page)
+        use(positionApplyPage )
+    }
+}
 
 export class PositionApplyPage extends PositionApplyLocators{
 
     constructor(page: Page) {
         super(page)
+    }
+
+    async fillApplicationForm(): Promise<void> {
+
     }
 }
--- tests/e2e/resume-applying.spec.ts
+++ tests/e2e/resume-applying.spec.ts
@@ -2,8 +2,16 @@ import { test } from '../../common/noah/fixtures'
 import { COMPANY_TAB_OPTIONS } from '../../common/noah/constants'
 
 
-test('Check header', async ({ page, welcomePage}) => {
+test('Check header', async ({
+                                page,
+                                welcomePage,
+                                careersPage,
+                                positionOverview
+                            }) => {
+    const positionName = 'Software Development Engineer in Test (SDET)'
     await page.goto('https://noah.com/')
     await welcomePage.mainHeader.selectOptionFromCompanyDropDown(COMPANY_TAB_OPTIONS.CAREERS)
+    await careersPage.openPositionByName(positionName)
+    await positionOverview.applyButton.click()
     await page.pause()
 })
