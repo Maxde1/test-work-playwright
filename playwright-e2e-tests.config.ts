a5e59aa6398e279e7c2c5be255a17cff6893282b
Maksym Denysenko <maxde@gmail.com>
2023-07-17 15:54:45 +0300

    add ts e2e config

---
+++ playwright-e2e-tests.config.ts
@@ -0,0 +1,34 @@
+import { PlaywrightTestConfig } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+    timeout: 60000,
+    retries: 1,
+    use: {
+        headless: false,
+        viewport: { width: 1280, height: 720 },
+        actionTimeout: 15000,
+        ignoreHTTPSErrors: true,
+        video: 'off',
+        screenshot: 'on'
+    },
+    projects: [
+        {
+            name: 'E2E testing with chromium',
+            use: {
+                browserName: 'chromium',
+                viewport:  { width: 1920, height: 1080 }
+            }
+
+        },
+        {
+            name: 'E2E testing with firefox',
+            use: { browserName: "firefox"}
+        },
+        {
+            name: "E2E testing with Webkit",
+            use: { browserName: "webkit"}
+        }
+    ]
+}
+
+export default config
