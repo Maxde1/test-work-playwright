aa2aaac279fa028e76b50af06dc9cf4fa7ff2054
Maksym Denysenko <maxde@gmail.com>
2023-07-17 22:17:18 +0300

    refactor test

--- tests/e2e/resume-applying.spec.ts
+++ tests/e2e/resume-applying.spec.ts
@@ -1,17 +1,26 @@
-import { test } from '../../common/noah/fixtures'
-import { COMPANY_TAB_OPTIONS } from '../../common/noah/constants'
+import { test } from '../../app/noah/common/fixtures'
+import { COMPANY_TAB_OPTIONS } from '../../app/noah/common/constants'
+import { resumeEntries } from '../../app/noah/data-repository/resume-entries'
+import { PositionApplyPage } from '../../app/noah/pages/position-apply.po'
+import { PositionOverviewPage } from '../../app/noah/pages/position-overview.po'
+import { logger } from '../../app/noah/common/logger'
 
+test.beforeEach(async ({ page }, testInfo) => {
+    logger.info('Visit welcome page')
+    await page.goto('/');
+});
 
 test('Check header', async ({
-                                page,
+                                context,
                                 welcomePage,
                                 careersPage,
-                                positionOverview
                             }) => {
     const positionName = 'Software Development Engineer in Test (SDET)'
-    await page.goto('https://noah.com/')
+    const resumeEntity = await resumeEntries.getResumeOnlyWithRequiredFields()
     await welcomePage.mainHeader.selectOptionFromCompanyDropDown(COMPANY_TAB_OPTIONS.CAREERS)
-    await careersPage.openPositionByName(positionName)
-    await positionOverview.applyButton.click()
-    await page.pause()
+    const newPage = await careersPage.openPositionByName(positionName, context)
+    const positionOverviewPage = new PositionOverviewPage(newPage)
+    await positionOverviewPage.applyButton.click()
+    const positionApplyPage = new PositionApplyPage(newPage)
+    await positionApplyPage.fillApplicationForm(resumeEntity)
 })
