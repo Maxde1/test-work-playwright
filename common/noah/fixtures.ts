09175e7ca9d7a0ed6d4f8aa4c15f22a2d9bd4ffe
Maksym Denysenko <maxde@gmail.com>
2023-07-17 16:27:57 +0300

    extend fixture file

--- common/noah/fixtures.ts
+++ common/noah/fixtures.ts
@@ -1,13 +1,19 @@
 import { test as base } from '@playwright/test'
 import { mainHeaderComponent, MainHeaderComponent } from './components/main-header.co'
 import { welcomePage, WelcomePage } from './pages/welcome.po'
+import { careersPage, CareersPage } from './pages/careers.po'
+import { positionOverviewPage, PositionOverviewPage } from './pages/position-overview.po'
 
 export const test = base.extend<{
     mainHeaderComponent: MainHeaderComponent
     welcomePage: WelcomePage
+    careersPage: CareersPage
+    positionOverview: PositionOverviewPage
 }>({
     ...mainHeaderComponent,
-    ...welcomePage
+    ...welcomePage,
+    ...careersPage,
+    ...positionOverviewPage
 })
 
 export { expect } from '@playwright/test';
