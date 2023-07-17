e8562e3e7052f70830a3291d1c891e1fd6a8a030
Maksym Denysenko <maxde@gmail.com>
2023-07-17 15:55:37 +0300

    create constants file and add couple const

---
+++ common/noah/constants.ts
@@ -0,0 +1,12 @@
+export enum COMPANY_TAB_OPTIONS {
+    ABOUT = "About",
+    TEAM = "Team",
+    CAREERS = "Careers"
+}
+
+export enum HEADER_TABS {
+    COMPANY = "Company",
+    FEATURES = "Features",
+    SUPPORT = "Support",
+    BLOG = "Blog"
+}
